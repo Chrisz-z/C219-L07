baf39663e6b8d37ae2c628a092477d7c33b4cccd
Chrisz-z <22035357@myrp.edu.sg>
2023-11-28 22:32:27 +0800

    dome

---
+++ tags.js
@@ -0,0 +1,14 @@
+var searchTags = [
+    "HTML",
+    "CSS",
+    "PHP",
+    "JavaScript",
+    "Python",
+    "Java",
+    "iOS",
+    "Swift",
+    "React",
+    "Web Design",
+    "Web Development"
+];
+
